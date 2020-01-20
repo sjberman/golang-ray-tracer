1673fc05a42a5ea187deabdbfbe4ce5152764bc7
Saylor Berman <s.berman@f5.com>
2020-01-20 16:45:14 -0700

    Clean up main and add demo file for sandboxing

---
+++ demo/demo.go
@@ -0,0 +1,125 @@
+package demo
+
+import (
+	"math"
+
+	. "github.com/sjberman/golang-ray-tracer/pkg/base"
+	. "github.com/sjberman/golang-ray-tracer/pkg/image"
+	. "github.com/sjberman/golang-ray-tracer/pkg/scene"
+	. "github.com/sjberman/golang-ray-tracer/pkg/scene/object"
+)
+
+func FiveBallRainbow(camera *Camera) (*Group, []*PointLight) {
+	s1 := NewSphere()
+	s1.Color = NewColor(1, 0, 0)
+	s1.Reflective = 0.9
+
+	s2 := NewSphere()
+	s2.SetTransform(
+		Scale(0.5, 0.5, 0.5),
+		Translate(-3.5, 0, 0),
+	)
+	s2.Color = NewColor(0, 1, 1)
+	s2.Reflective = 0.9
+
+	s3 := NewSphere()
+	s3.SetTransform(
+		Scale(0.5, 0.5, 0.5),
+		Translate(0, -3.5, 0),
+	)
+	s3.Color = NewColor(0.1, 0.4, 1)
+	s3.Reflective = 0.9
+
+	s4 := NewSphere()
+	s4.SetTransform(
+		Scale(0.5, 0.5, 0.5),
+		Translate(3.5, 0, 0),
+	)
+	s4.Color = NewColor(1, 1, 0)
+	s4.Reflective = 0.9
+
+	s5 := NewSphere()
+	s5.SetTransform(
+		Scale(0.5, 0.5, 0.5),
+		Translate(0, 3.5, 0),
+	)
+	s5.Color = NewColor(0.6, 1, 0)
+	s5.Reflective = 0.9
+
+	group := NewGroup()
+	group.Add(
+		s1,
+		s2,
+		s3,
+		s4,
+		s5,
+	)
+	camera.SetTransform(ViewTransform(NewPoint(0, 0, -5), NewPoint(0, 0, 0), NewVector(0, 1, 0)))
+	lights := []*PointLight{
+		NewPointLight(NewPoint(-6, 6, -5), White),
+		NewPointLight(NewPoint(10, -10, 3), White),
+	}
+	return group, lights
+}
+
+func AllShapes(camera *Camera) (*Group, []*PointLight) {
+	s1 := NewSphere()
+	s1.SetTransform(
+		Translate(0, 2, 0),
+	)
+	s1.Color = NewColor(1, 0, 0)
+
+	s2 := NewSphere()
+	s2.SetTransform(
+		Translate(-2.5, 2, -1),
+	)
+	s2.Color = NewColor(0, 1, 0)
+
+	s3 := NewSphere()
+	s3.SetTransform(
+		Translate(2.5, 2, -1),
+	)
+	s3.Color = NewColor(0, 0, 1)
+
+	c1 := NewCylinder()
+	c1.Closed = true
+	c1.Maximum = 1
+	c1.Minimum = -1
+	c1.SetTransform(
+		Translate(-2.5, 0, -1),
+	)
+	c1.Color = NewColor(0.5, 0.2, 0.7)
+	c1.Reflective = 0.1
+
+	c2 := NewCube()
+	c2.Color = NewColor(0.2, 0.7, 0.4)
+	c2.SetTransform(
+		RotateY(math.Pi / 6),
+	)
+	c2.Reflective = 0.2
+
+	c3 := NewCone()
+	c3.Closed = true
+	c3.Maximum = 1
+	c3.Minimum = -1
+	c3.SetTransform(
+		Translate(2.5, 0, -1),
+	)
+	c3.Color = NewColor(0.8, 0.3, 0.4)
+	c3.Reflective = 0.5
+
+	group := NewGroup()
+	group.Add(
+		s1,
+		s2,
+		s3,
+		c1,
+		c2,
+		c3,
+	)
+	camera.SetTransform(ViewTransform(NewPoint(0, 5, -10), NewPoint(0, 0, 0), NewVector(0, 1, 0)))
+	lights := []*PointLight{
+		NewPointLight(NewPoint(-5, 10, -10), White),
+	}
+	return group, lights
+}
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"runtime/pprof"
 	"time"
 
+	"github.com/sjberman/golang-ray-tracer/demo"
 	. "github.com/sjberman/golang-ray-tracer/pkg/base"
 	. "github.com/sjberman/golang-ray-tracer/pkg/image"
 	. "github.com/sjberman/golang-ray-tracer/pkg/parser"
@@ -35,94 +36,8 @@ func usingObjFile(objfile *string, camera *Camera) (*Group, []*PointLight) {
 }
 
 func usingCustomObjects(camera *Camera) (*Group, []*PointLight) {
-	// r1 := NewSphere()
-	// r1.SetTransform(
-	// 	Translate(6, 1, 4),
-	// )
-	// r1.Color = NewColor(1, 0.3, 0.2)
-	// r1.Specular = 0.4
-	// r1.Shininess = 5
-
-	// r2 := NewSphere()
-	// r2.SetMaterial(r1.GetMaterial())
-	// r2.SetTransform(
-	// 	Translate(2, 1, 3),
-	// )
-
-	// r3 := NewSphere()
-	// r3.SetMaterial(r1.GetMaterial())
-	// r3.SetTransform(
-	// 	Translate(-1, 1, 2),
-	// )
-
-	// blueSphere := GlassSphere()
-	// blueSphere.Color = NewColor(0, 0, 0.2)
-	// blueSphere.Diffuse = 0.4
-	// blueSphere.Specular = 0.9
-	// blueSphere.Reflective = 0.9
-	// blueSphere.Shininess = 300
-	// blueSphere.SetTransform(
-	// 	Translate(0.6, 0.7, -0.6),
-	// 	Scale(0.7, 0.7, 0.7),
-	// )
-
-	// greenSphere := GlassSphere()
-	// greenSphere.SetMaterial(blueSphere.GetMaterial())
-	// greenSphere.Color = NewColor(0, 0.2, 0)
-	// greenSphere.SetTransform(
-	// 	Translate(-0.7, 0.5, -0.8),
-	// 	Scale(0.5, 0.5, 0.5),
-	// )
-	s1 := NewSphere()
-	s1.Color = NewColor(1, 0, 0)
-	s1.Reflective = 0.9
-
-	s2 := NewSphere()
-	s2.SetTransform(
-		Scale(0.5, 0.5, 0.5),
-		Translate(-3.5, 0, 0),
-	)
-	s2.Color = NewColor(0, 1, 1)
-	s2.Reflective = 0.9
-
-	s3 := NewSphere()
-	s3.SetTransform(
-		Scale(0.5, 0.5, 0.5),
-		Translate(0, -3.5, 0),
-	)
-	s3.Color = NewColor(0.1, 0.4, 1)
-	s3.Reflective = 0.9
-
-	s4 := NewSphere()
-	s4.SetTransform(
-		Scale(0.5, 0.5, 0.5),
-		Translate(3.5, 0, 0),
-	)
-	s4.Color = NewColor(1, 1, 0)
-	s4.Reflective = 0.9
-
-	s5 := NewSphere()
-	s5.SetTransform(
-		Scale(0.5, 0.5, 0.5),
-		Translate(0, 3.5, 0),
-	)
-	s5.Color = NewColor(0.6, 1, 0)
-	s5.Reflective = 0.9
-
-	group := NewGroup()
-	group.Add(
-		s1,
-		s2,
-		s3,
-		s4,
-		s5,
-	)
-	camera.SetTransform(ViewTransform(NewPoint(0, 0, -5), NewPoint(0, 0, 0), NewVector(0, 1, 0)))
-	lights := []*PointLight{
-		NewPointLight(NewPoint(-6, 6, -5), White),
-		NewPointLight(NewPoint(10, -10, 3), White),
-	}
-	return group, lights
+	// return demo.FiveBallRainbow(camera)
+	return demo.AllShapes(camera)
 }
 
 // Test program
@@ -134,16 +49,24 @@ func main() {
 
 	flag.Parse()
 
-	camera := NewCamera(1500, 1500, math.Pi/3)
+	camera := NewCamera(1200, 1200, math.Pi/3)
 
-	cp := NewRingPattern(Black, White)
+	cp := NewCheckerPattern(Black, White)
 	cp.SetTransform(RotateY(math.Sqrt(2) / 2))
 	floor := NewPlane()
 	floor.SetTransform(
-		Translate(0, -5, 0),
+		Translate(0, -1, 0),
 	)
 	floor.Pattern = cp
-	floor.Reflective = 0.4
+	floor.Reflective = 0.2
+
+	wall := NewPlane()
+	rp := NewRingPattern(Black, White)
+	wall.SetTransform(
+		RotateX(math.Pi/2),
+		Translate(0, 5, 0),
+	)
+	wall.Pattern = rp
 
 	// Build scene using OBJ file
 	// group, lights := usingObjFile(objFile, camera)
@@ -156,7 +79,11 @@ func main() {
 
 	group.Divide(1)
 
-	world := NewWorld(lights, []Object{group})
+	world := NewWorld(lights, []Object{
+		group,
+		floor,
+		wall,
+	})
 
 	canvas := Render(camera, world)
 	err := canvas.WriteToFile("image.ppm")
